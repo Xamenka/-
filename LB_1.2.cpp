0733533421ec94acac8313416017bd793892c551
Xamenka <xoma1007@gmail.com>
2024-11-29 22:11:40 +0200

    Update LB_1.2.cpp

--- LB_1.2.cpp
+++ LB_1.2.cpp
@@ -1,30 +1,26 @@
-//Варіант 2
 #include <iostream>
-#include <cmath>    // для математичних функцій
+#include <cmath>    
 
 using namespace std;
 
 int main() {
-    // Задані значення змінних
     double a = 2;
     double b = 14.36;
 
-    //Обчислення необхідних значень
-    double ln_a = log(a);            // натуральний логарифм a
-    double cbrt_b = cbrt(b);         // кубічний корінь з b
-    double cos_b = cos(b);           // косинус b
-    double exp_a = exp(a);           // експонента a
-    double a_pow_a = pow(a, a);      // a в степені a
+    double ln_a = log(a);            // Г­Г ГІГіГ°Г Г«ГјГ­ГЁГ© Г«Г®ГЈГ Г°ГЁГґГ¬ a
+    double cbrt_b = cbrt(b);         // ГЄГіГЎВіГ·Г­ГЁГ© ГЄГ®Г°ВіГ­Гј Г§ b
+    double cos_b = cos(b);           // ГЄГ®Г±ГЁГ­ГіГ± b
+    double exp_a = exp(a);           // ГҐГЄГ±ГЇГ®Г­ГҐГ­ГІГ  a
+    double a_pow_a = pow(a, a);      // a Гў Г±ГІГҐГЇГҐГ­Ві a
 
-    //Обчислення чисельника та знаменника
+  
     double numerator = 8.15 * cbrt_b * ln_a;
     double denominator = 24.38 * cos_b * (exp_a - a_pow_a);
 
-    //Остаточне обчислення y
     double y = numerator / denominator;
 
-    //Виведення результату
-    cout << "Значення функції y = " << y << endl;
+ 
+    cout << "Г‡Г­Г Г·ГҐГ­Г­Гї ГґГіГ­ГЄГ¶ВіВї y = " << y << endl;
 
     return 0;
 }
