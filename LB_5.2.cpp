14432cc38f382fedf12862c78d1004fc65193692
Xamenka <xoma1007@gmail.com>
2024-11-29 22:17:44 +0200

    Update LB_5.2.cpp

--- LB_5.2.cpp
+++ LB_5.2.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <cmath> // для функції sqrt
-
+#include <cmath>
 using namespace std;
 
 int main() {
-    double sum = 0.0; // Сума квадратних коренів
-    int count = 20;   // Кількість чисел
+    double sum = 0.0;
+    int count = 20;   
 
-    // Цикл для обчислення суми квадратних коренів
     for (int i = 1; i <= count; ++i) {
-        sum += sqrt(i); // Додаємо квадратний корінь до суми
+        sum += sqrt(i); 
     }
 
-    // Обчислюємо середнє арифметичне
+   
     double average = sum / count;
 
-    // Виводимо результат
+   
     cout << "The arithmetic mean of the roots of numbers from 1 to 20: " << average << endl;
 
     return 0;
