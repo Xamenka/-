e48c3d836b264051a2e2c98f24fc908a9e93e1ec
Xamenka <xoma1007@gmail.com>
2024-11-29 22:18:31 +0200

    Update LB_6.cpp

--- LB_6.cpp
+++ LB_6.cpp
@@ -5,20 +5,20 @@ using namespace std;
 int main() {
     int N, digit, sum = 0;
 
-    // Введення числа N з клавіатури
+   
     cout << "Enter a positive integer N: ";
     cin >> N;
 
-    // Розклад числа на цифри та знаходження суми цифр, більших за 5
+  
     while (N > 0) {
-        digit = N % 10;  // Отримуємо останню цифру
+        digit = N % 10;  
         if (digit > 5) {
-            sum += digit;  // Якщо цифра більше 5, додаємо її до суми
+            sum += digit;  
         }
-        N /= 10;  // Видаляємо останню цифру
+        N /= 10;  
     }
 
-    // Виведення результату
+  
     cout << "The sum of digits greater than 5: " << sum << endl;
 
     return 0;
