3216f5c804c226b898abd24223eeaaf111235808
Xamenka <xoma1007@gmail.com>
2024-11-29 22:18:10 +0200

    Update LB_5.3.cpp

--- LB_5.3.cpp
+++ LB_5.3.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// Оголошення функції для обчислення y
+
 double func(double x) {
     return pow(x, 5) - sqrt(9 - x * x);
 }
@@ -12,7 +12,7 @@ double func(double x) {
 int main() {
     double a, b, h;
 
-    // Введення початкових значень з клавіатури
+   
     cout << "Enter a: ";
     cin >> a;
     cout << "Enter b: ";
@@ -20,7 +20,7 @@ int main() {
     cout << "Enter h: ";
     cin >> h;
 
-    // Перевірка на правильність введення
+  
     if (a > b) {
         cout << "The starting point cannot be greater than the ending point!" << endl;
         return 1;
@@ -30,15 +30,13 @@ int main() {
     cout << "|   X   |     Y       |" << endl;
     cout << "-----------------------" << endl;
 
-    // Цикл для табулювання значень
+   
     for (double x = a; x <= b; x += h) {
-        // Перевірка на область визначення функції
         if (x < -3 || x > 3) {
             cout << "x = " << x << " does not belong to the ODZ!" << endl;
             break;
         }
 
-        // Виведення результатів у вигляді таблиці
         cout << "| " << setw(5) << x << " | " << setw(10) << func(x) << " |" << endl;
     }
 
