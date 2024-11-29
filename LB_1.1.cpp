48b5dfbf1165d2bdfe9017db954b56e8dc2e26b4
Xamenka <xoma1007@gmail.com>
2024-11-29 22:10:37 +0200

    Update LB_1.1.cpp

--- LB_1.1.cpp
+++ LB_1.1.cpp
@@ -1,30 +1,26 @@
-// Варіант 22
 #include <iostream>
-#include <cmath>    // для використання математичних функцій
+#include <cmath>    
 
 using namespace std;
 
 int main() {
-    // Задане значення змінних
     double a = 2;
     double b = 3.141;
 
-    //Обчислення логарифмів
     double ln_a = log(a);
     double ln_b = log(b);
     double ln_b_a = log(b / a);
 
-    //Обчислення складових всередині формули
     double part1 = ln_b / 0.711;
     double part2 = ln_b_a / (1 + part1);
     double part3 = cbrt(1 + part2);
     double part4 = cbrt(1 + part3);
 
-    //Остаточне обчислення y
+    //ГЋГ±ГІГ ГІГ®Г·Г­ГҐ Г®ГЎГ·ГЁГ±Г«ГҐГ­Г­Гї y
     double y = part4 * (ln_a / 0.315);
 
-    //Виведення результату
-    cout << "Значення функції y = " << y << endl;
+    //Г‚ГЁГўГҐГ¤ГҐГ­Г­Гї Г°ГҐГ§ГіГ«ГјГІГ ГІГі
+    cout << "Г‡Г­Г Г·ГҐГ­Г­Гї ГґГіГ­ГЄГ¶ВіВї y = " << y << endl;
 
     return 0;
 }
