0b19327a78277d6abd94a17d3f80e9b4e11cb662
Xamenka <xoma1007@gmail.com>
2025-06-04 15:21:35 +0300

    Update task1.cpp

--- LB_26/task1.cpp
+++ LB_26/task1.cpp
@@ -3,34 +3,27 @@
 #include <string>
 using namespace std;
 
-// Базовий клас IPAddress
 class IPAddress {
 protected:
     string address;
 
 public:
-    // Конструктор
     IPAddress(string addr) : address(addr) {}
 
-    // Копі-конструктор
     IPAddress(const IPAddress& other) {
         address = other.address;
     }
 
-    // Віртуальний метод для виводу
     virtual void print() {
         cout << address;
     }
 
-    // Віртуальний деструктор
     virtual ~IPAddress() {}
 };
 
-// Похідний клас IPAddressChecked
 class IPAddressChecked : public IPAddress {
     bool isCorrect;
 
-    // Допоміжна функція для перевірки IP
     bool validateIP(const string& addr) {
         istringstream ss(addr);
         string token;
@@ -52,17 +45,14 @@ class IPAddressChecked : public IPAddress {
     }
 
 public:
-    // Конструктор
     IPAddressChecked(string addr) : IPAddress(addr) {
         isCorrect = validateIP(addr);
     }
 
-    // Копі-конструктор
     IPAddressChecked(const IPAddressChecked& other) : IPAddress(other) {
         isCorrect = other.isCorrect;
     }
 
-    // Перевизначення методу print
     void print() override {
         IPAddress::print(); // виклик методу з базового класу
         cout << " - " << (isCorrect ? "Correct" : "Not Correct");
@@ -72,16 +62,13 @@ public:
 int main() {
     string ip1, ip2, ip3;
 
-    // Ввід з клавіатури
     cout << "Enter three IP addresses:\n";
     cin >> ip1 >> ip2 >> ip3;
 
-    // Створення об'єктів
     IPAddress ip(ip1);
     IPAddressChecked ipChecked1(ip2);
     IPAddressChecked ipChecked2(ip3);
 
-    // Вивід результатів
     ip.print(); cout << endl;
     ipChecked1.print(); cout << endl;
     ipChecked2.print(); cout << endl;
