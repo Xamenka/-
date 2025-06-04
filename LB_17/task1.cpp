cf6dc4f2d5570c34be84ae83250e166e97d92ca3
Xamenka <xoma1007@gmail.com>
2025-06-04 15:13:11 +0300

    Update task1.cpp

--- LB_17/task1.cpp
+++ LB_17/task1.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 bool isPrime(int num) {
-    if (num < 2) return false; // Числа менше 2 не є натуральними 
+    if (num < 2) return false; 
     for (int i = 2; i * i <= num; i++) { 
-        if (num % i == 0) return false; //Якщо ділиться без остатку то не є натуральне 
+        if (num % i == 0) return false; 
     }
-    return true; // Если не нашли делителей, число простое
+    return true; 
 }
 
 int main() {
