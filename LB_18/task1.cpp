c6b7cfb4d1b0ee03c97561492792e32d98521b9a
Xamenka <xoma1007@gmail.com>
2025-06-04 15:13:33 +0300

    Update task1.cpp

--- LB_18/task1.cpp
+++ LB_18/task1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void increment(int &var) {  // var посилання на змінну          
+void increment(int &var) {        
     var += 1; 
 }
 
-void increment(int &var, int value) {   // коли збільшуєм var в функції ми змінюємо зміну яку ми передали а не її копію 
+void increment(int &var, int value) {   
     var += value; 
 }
 
 int main() {
-    int var = 0;                            //якщо кратне + це число , якщо не кратне + 1
+    int var = 0;                            
     for (int i = 0; i < 10; i++) {
         if (i % 2)
             increment(var);
