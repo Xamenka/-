2be9199d736d6224252d0851c61627b9208823d8
Xamenka <xoma1007@gmail.com>
2025-06-04 15:19:21 +0300

    Update task1.cpp

--- LB_23/task1.cpp
+++ LB_23/task1.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-// Пошук найбільшого спільного дільника (НСД)
 int gcd(int a, int b) {
     if (b == 0)
         return abs(a);
@@ -15,21 +14,20 @@ int gcd(int a, int b) {
 class Fraction {
 public:
     Fraction(int numerator, int denominator);
-    string toString();         // Повертає рядкове представлення дробу
-    double toDouble();         // Повертає десяткове представлення дробу
+    string toString();       
+    double toDouble();
 
-    Fraction plus(Fraction that);    // Додавання
-    Fraction minus(Fraction that);   // Віднімання
-    Fraction times(Fraction that);   // Множення
-    Fraction by(Fraction that);      // Ділення
+    Fraction plus(Fraction that);   
+    Fraction minus(Fraction that);  
+    Fraction times(Fraction that); 
+    Fraction by(Fraction that);     
 
 private:
     int numerator;
     int denominator;
-    void reduce();  // Скорочення дробу
+    void reduce();  
 };
 
-// Конструктор дробу з перевіркою на поділ на нуль
 Fraction::Fraction(int n, int d) : numerator(n), denominator(d) {
     if (denominator == 0) {
         cout << "Error: division by zero is not allowed." << endl;
@@ -38,41 +36,35 @@ Fraction::Fraction(int n, int d) : numerator(n), denominator(d) {
     reduce(); // Скорочуємо дріб
 }
 
-// Метод скорочення дробу
 void Fraction::reduce() {
     int divisor = gcd(numerator, denominator);
     numerator /= divisor;
     denominator /= divisor;
 
-    // Забезпечуємо, що знаменник завжди додатній
     if (denominator < 0) {
         numerator = -numerator;
         denominator = -denominator;
     }
 }
 
-// Додавання дробів
 Fraction Fraction::plus(Fraction that) {
     int num = this->numerator * that.denominator + that.numerator * this->denominator;
     int den = this->denominator * that.denominator;
     return Fraction(num, den);
 }
 
-// Віднімання дробів
 Fraction Fraction::minus(Fraction that) {
     int num = this->numerator * that.denominator - that.numerator * this->denominator;
     int den = this->denominator * that.denominator;
     return Fraction(num, den);
 }
 
-// Множення дробів
 Fraction Fraction::times(Fraction that) {
     int num = this->numerator * that.numerator;
     int den = this->denominator * that.denominator;
     return Fraction(num, den);
 }
 
-// Ділення дробів
 Fraction Fraction::by(Fraction that) {
     if (that.numerator == 0) {
         cout << "Error: cannot divide by a fraction with numerator zero." << endl;
@@ -83,7 +75,6 @@ Fraction Fraction::by(Fraction that) {
     return Fraction(num, den);
 }
 
-// Перетворення дробу у зручний рядковий вигляд
 string Fraction::toString() {
     int whole = numerator / denominator;
     int rem = abs(numerator % denominator);
@@ -97,7 +88,6 @@ string Fraction::toString() {
     }
 }
 
-// Перетворення дробу у десятковий формат
 double Fraction::toDouble() {
     return static_cast<double>(numerator) / denominator;
 }
@@ -106,25 +96,20 @@ int main() {
     string input1, input2;
     int n1, d1, n2, d2;
 
-    // Ввід дробів
     cout << "Enter two fractions (example: 3/4 5/6): ";
     cin >> input1 >> input2;
 
-    // Розділення першого дробу
     size_t slash_pos1 = input1.find('/');
     n1 = stoi(input1.substr(0, slash_pos1));
     d1 = stoi(input1.substr(slash_pos1 + 1));
 
-    // Розділення другого дробу
     size_t slash_pos2 = input2.find('/');
     n2 = stoi(input2.substr(0, slash_pos2));
     d2 = stoi(input2.substr(slash_pos2 + 1));
 
-    // Створення об'єктів дробів
     Fraction f1(n1, d1);
     Fraction f2(n2, d2);
 
-    // Вивід результатів усіх операцій
     cout << f1.toString() << " + " << f2.toString() << " = " << f1.plus(f2).toString() << endl;
     cout << f1.toString() << " - " << f2.toString() << " = " << f1.minus(f2).toString() << endl;
     cout << f1.toString() << " * " << f2.toString() << " = " << f1.times(f2).toString() << endl;
