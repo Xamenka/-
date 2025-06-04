3c9e33172b3dd2aff59b74bcde43bfc6f2224af7
Xamenka <xoma1007@gmail.com>
2025-06-04 15:22:16 +0300

    Update task2.cpp

--- LB_27/task2.cpp
+++ LB_27/task2.cpp
@@ -5,19 +5,17 @@ int main(void) {
     int a = 0, b = 0, c = 0;
     
     cout << "Enter two integers (dividend and divisor):\n";
-    cin >> a;  // Вводимо чисельник
-    cin >> b;  // Вводимо дільник
+    cin >> a; 
+    cin >> b;  
 
     try {
         if (b == 0) {
-            // Викидаємо виняток, якщо дільник 0
             throw "Your input is not valid, you can't divide by zero.";
         }
-        c = a / b;  // Ділення
-        cout << "Result: " << c << endl;  // Виведення результату
+        c = a / b;  
+        cout << "Result: " << c << endl;  
     }
     catch (const char* msg) {
-        // Перехоплюємо виняток та виводимо повідомлення
         cout << msg << endl;
     }
 
