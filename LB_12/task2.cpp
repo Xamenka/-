d82b2753c0c5d92ad79a2dc19ebe99065bab548e
Xamenka <xoma1007@gmail.com>
2025-06-04 15:08:56 +0300

    Update task2.cpp

--- LB_12/task2.cpp
+++ LB_12/task2.cpp
@@ -4,8 +4,7 @@
 int main() {
     const int rows = 10, cols = 10;
     int matrix[rows][cols] = {0};
-    int *ptr = &matrix[0][0];//* є вказівником і буде використовуватися для зберігання інформації про місцезнаходження даних типу int. 
-    //&Оператор просто знаходить адресу свого аргументу.
+    int *ptr = &matrix[0][0];
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             *(ptr + i * cols + j) = (i + 1) * (j + 1);
